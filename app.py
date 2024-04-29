9cd04585e143c9540caba269d056d282da44313c
F-KAD <kfaiza2002@yahoo.com>
2024-04-29 14:21:19 +0200

    Modif port app.py

--- app.py
+++ app.py
@@ -59,6 +59,11 @@ def predict():
     else:
         return jsonify({'error': 'No tweet provided'}), 400
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     #app.run(port=8000, debug=True)
-    app.run()
+    #app.run()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
