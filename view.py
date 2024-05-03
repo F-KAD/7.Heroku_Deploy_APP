4cdb1ca8ec9f8fb18dddafe7f9abf6bc089a8b9e
F-KAD <kfaiza2002@yahoo.com>
2024-05-03 14:18:39 +0200

    Test View

--- view.py
+++ view.py
@@ -1,10 +1,12 @@
-from flask import Flask
+import json
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world !"
+    #return "Hello world !"
+    return render_template_string("<h1>"Hello world !"<h1> <br>")
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
