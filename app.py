aeb8c16745a67d0508669a737db7945876fb2a2c
F-KAD <kfaiza2002@yahoo.com>
2024-05-03 07:20:15 +0200

    MODIF app.py avec le port d'ecoute

--- app.py
+++ app.py
@@ -75,5 +75,10 @@ def predict():
         return jsonify({'error': 'No tweet provided'}), 400
 
 
+#if __name__ == "__main__":
+ #   app.run()
+
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
