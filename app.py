83136c5b472f2cc98707b4c5d0119b9c90026eb7
F-KAD <kfaiza2002@yahoo.com>
2024-05-04 21:13:38 +0200

    For dynos optimisation MODIF app suppresuib subprocess

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 import spacy
-#import subprocess
 from flask import Flask, request, jsonify
 from nltk.stem import PorterStemmer
 from keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 import pickle
+import tensorflow as tf
 
 app = Flask(__name__)
 nlp = spacy.load("en_core_web_sm")
+model = None
+tokenizer = None
 
-# Installation du modèle spaCy
-#subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+def load_sentiment_model():
+    global model
+    if model is None:
+        model = load_model("model_LSTM_Stem_Glove_Emb_Final_Sentiment_Analysis")
 
-# Charger le tokenizer et le modèle
-with open('tokenizer.pickle', 'rb') as handle:
-    tokenizer = pickle.load(handle)
-model = load_model("model_LSTM_Stem_Glove_Emb_Final_Sentiment_Analysis")
+def load_tokenizer():
+    global tokenizer
+    if tokenizer is None:
+        with open('tokenizer.pickle', 'rb') as handle:
+            tokenizer = pickle.load(handle)
 
 def tweet_clean(tweet):
     clean = " ".join([PorterStemmer().stem(token.text) for token in nlp(tweet)])
@@ -30,6 +35,7 @@ def tweet_padded(tweet):
 def tweet_predict(tweet):
     twt_padded = tweet_padded(tweet)
     pred = model.predict(twt_padded)[0][0]
+    del twt_padded  # Libérer la mémoire
     return pred
 
 def tweet_sentiment(pred):
@@ -47,6 +53,8 @@ def predict():
     tweet = request.args.get('tweet')
     if tweet:
         try:
+            load_sentiment_model()
+            load_tokenizer()
             pred = tweet_predict(tweet)
             sentiment = tweet_sentiment(pred)
             return jsonify({'prediction': pred, 'sentiment': sentiment}), 200
